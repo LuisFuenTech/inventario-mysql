8f5901ed738fd731994553fdb429d0879073d60f
Luis Fuentes <luisfuau10@gmail.com>
2019-06-21 23:27:22 -0500

    Clase producto lista

--- invetario_mysql/src/classes/Producto.java
+++ invetario_mysql/src/classes/Producto.java
@@ -12,4 +12,61 @@ package classes;
 public class Producto {
     //Llenar los atributos (columnas en DDBB) de esta clase
     //Esta representa la tabla 'producto' en la DB
+    private int id;
+    private String nombre;
+    private int cantidad;
+    private double costo_unitario;
+    private double costo_total;
+
+    public Producto() {        
+    }
+    
+    public Producto(int id, String nombre, int cantidad, double costo_unitario, double costo_total) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.costo_unitario = costo_unitario;
+        this.costo_total = costo_total;
+    }   
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+    
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+    
+    public double getCosto_unitario() {
+        return costo_unitario;
+    }
+
+    public void setCosto_unitario(double costo_unitario) {
+        this.costo_unitario = costo_unitario;
+    }
+
+    public double getCosto_total() {
+        return costo_total;
+    }
+
+    public void setCosto_total(double costo_total) {
+        this.costo_total = costo_total;
+    }
+     
 }
